0772f3f0a3315a89067b398ce3a989131721b7cc
Preet Patel <preet.patel@1Rintern.local>
2023-02-13 16:59:18 +0530

    Uploaded by Preet

---
+++ Account.java
@@ -0,0 +1,5 @@
+package BankAtmProject;
+
+public class Account {
+
+}
---
+++ BankAtm.java
@@ -0,0 +1,9 @@
+package BankAtmProject;
+
+public class BankAtm extends OptionMenu{
+
+	public static void main(String[] args) {
+		OptionMenu optionMenu = new OptionMenu();
+		optionMenu.getLogin();
+	}
+}
